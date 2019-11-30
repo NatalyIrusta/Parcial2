05b2b0e5c397ccf17df57a6a3d27597ccac9ad38
natalyirusta <natalyirusta@gmail.com>
2019-11-29 21:10:58 -0400

    Listo Finish

---
+++ Algarañaz/TestUnitarioDebit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarioDebit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarioDebit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e99f4e5b-91e6-428b-9910-565a57e8cf00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Algarañaz/TestUnitarioDebit/UnitTest2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Algarañaz;
+using Algarañaz.BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitarioDebit
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Miss Nataly Irusta", beginningBalance);
+
+            // Act
+            account.Credit(creditAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+        }
+    }
+}
